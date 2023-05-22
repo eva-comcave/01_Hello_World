4603046f0690a728b643d211ddc256014fc07968
Eva <e.moritz.comcave@gmail.com>
2023-05-22 09:31:11 +0200

    gea

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,9 +1,10 @@
 package com.cc.java;
 
-public class App {
+public class App {  //Immer alles in einer Klasse, so auch Dateiname
+  // Klassen werden gross geschrieben in java, alles andere klein
   
-    public static void main(String[] args) {
-      System.out.println("TGIF");  
+    public static void main(String[] args) {  // void = es gibt keinen return Wert
+      System.out.println("TGIF");  //Methode:Funktion in einer Klasse
     }
     
 }
